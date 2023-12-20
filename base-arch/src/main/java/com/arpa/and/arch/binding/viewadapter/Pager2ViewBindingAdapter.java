5fce00922d915206449d922096219cde3602f1b2
leergo <leergo@dingtalk.com>
2023-12-20 14:28:33 +0800

    ver: 1.2.4 调整Pager2ViewBindingAdapter
    Signed-off-by: leergo <leergo@dingtalk.com>

--- base-arch/src/main/java/com/arpa/and/arch/binding/viewadapter/Pager2ViewBindingAdapter.java
+++ base-arch/src/main/java/com/arpa/and/arch/binding/viewadapter/Pager2ViewBindingAdapter.java
@@ -1,16 +1,15 @@
 package com.arpa.and.arch.binding.viewadapter;
 
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-
-import com.arpa.and.arch.binding.adapter.SimpleFragmentStateAdapter;
-
-import java.util.List;
-
 import androidx.databinding.BindingAdapter;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.widget.ViewPager2;
+
+import com.arpa.and.arch.binding.adapter.SimpleFragmentStateAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.List;
 
 /**
  * author: 李一方(<a href="mailto:leergo@dingtalk.com">leergo@dingtalk.com</a>)<br/>
@@ -25,14 +24,15 @@ public class Pager2ViewBindingAdapter {
 
     @BindingAdapter("pageLimit")
     public static void setPageLimit(ViewPager2 viewPager, Integer pageLimit) {
-        if (null != pageLimit)
-            viewPager.setOffscreenPageLimit(pageLimit);
+        if (null != pageLimit) {viewPager.setOffscreenPageLimit(pageLimit);}
     }
 
-    @BindingAdapter({"fragments", "tab", "titles"})
+    @BindingAdapter(value = {"fragments", "tab", "titles"}, requireAll = false)
     public static void setFragments(ViewPager2 viewPager, final List<? extends Fragment> fragments, TabLayout tab, List<String> titles) {
         if (null != fragments) {
             viewPager.setAdapter(new SimpleFragmentStateAdapter((FragmentActivity) viewPager.getContext(), fragments));
+        }
+        if (null != tab && null != titles && !titles.isEmpty()) {
             new TabLayoutMediator(tab, viewPager, (v, position) -> v.setText(titles.get(position))).attach();
         }
     }
